d7ffe679d9782e89eb140a1b635d135348e08307
David-Pops <dpopowski6371@stu.d214.org>
2023-09-12 11:18:00 -0500

    Made the function for button actually word

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -9,95 +9,63 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    var body: some View {
+    var body: some View{
+        VStack{
+            Text("0")
+                .font(.largeTitle)
+                .frame(width: 380, height: 78, alignment: .trailing)
+                .background(.blue)
             
+            
+            
+            HStack{
+                numberButton(buttonNumber: "AC", buttonColor: .gray)
+                numberButton(buttonNumber: "+/-", buttonColor: .gray)
+                numberButton(buttonNumber: "%", buttonColor: .gray)
+                numberButton(buttonNumber: "÷", buttonColor: .orange)
+                
+            }
+            
+            
+            mainButtonRow(textOne: "7", textTwo: "8", textThree: "9", textFour: "X")
+            mainButtonRow(textOne: "4", textTwo: "5", textThree: "6", textFour: "-")
+            mainButtonRow(textOne: "1", textTwo: "2", textThree: "3", textFour: "+")
         
-        VStack {
-            HStack{
-                Circle()
-                    .foregroundColor(.gray)
-                Circle()
-                    .foregroundColor(.gray)
-                Circle()
-                    .foregroundColor(.gray)
-                Circle()
-                    .foregroundColor(.orange)
-                
-            }
-            HStack{
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.orange)
-                
-            }
-            HStack{
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.orange)
-                
-            }
-            HStack{
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.gray)
-                    .colorInvert()
-                Circle()
-                    .foregroundColor(.orange)
-                
-            }
-                        
-                
-                
             
         }
+        .background(.black)
     }
 }
 
-func numberButton(buttonNumber: String, buttonColor: Color){
+
+
+
+func numberButton(buttonNumber: String, buttonColor: Color) -> some View{
     var body : some View{
         ZStack{
             Circle()
                 .foregroundColor(buttonColor)
             Text("\(buttonNumber)")
+                .font(.largeTitle)
         }
     }
+    return body
 }
 
-func mainButtonRow(textOne: String, textTwo: String, textThree: String, textFour: String){
+func mainButtonRow(textOne: String, textTwo: String, textThree: String, textFour: String) -> some View{
     var body : some View{
         HStack{
-            //numberButton(buttonNumber: textOne, buttonColor: .gray)
-            //.colorInvert()
-            //numberButton(buttonNumber: textTwo, buttonColor: .gray)
-            //.colorInvert()
-            //numberButton(buttonNumber: textThree, buttonColor: .gray)
-            //.colorInvert()
-            //numberButton(buttonNumber: textFour, buttonColor: .orange)
+            numberButton(buttonNumber: textOne, buttonColor: .gray)
+                .colorInvert()
+            numberButton(buttonNumber: textTwo, buttonColor: .gray)
+                .colorInvert()
+            numberButton(buttonNumber: textThree, buttonColor: .gray)
+                .colorInvert()
+            numberButton(buttonNumber: textFour, buttonColor: .orange)
         }
     }
+    return body
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
